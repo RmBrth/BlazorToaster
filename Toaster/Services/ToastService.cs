440f68e962971ce068a97423603095b5c7159f1d
RmBrth <85992948+RmBrth@users.noreply.github.com>
2021-07-11 00:23:05 +0200

    Ajoutez des fichiers projet.

---
+++ Toaster/Services/ToastService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Toaster.Services
+{
+    public class ToastService
+    {
+        public event Action<string> OnShow;
+
+        public void ShowToast(string message)
+        {
+            OnShow?.Invoke(message);
+        }
+    }
+}
---
+++ Toaster/Shared/Toast.razor.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Components;
+using Toaster.Services;
+
+namespace Toaster.Shared
+{
+    public partial class Toast
+    {
+        [Inject] ToastService ToastService { get; set; }
+
+        private bool displayed;
+        private string message;
+
+        protected override void OnInitialized()
+        {
+            ToastService.OnShow += Show;
+        }
+
+        public void Show(string message)
+        {
+            this.message = message;
+            displayed = true;
+            StateHasChanged();
+        }
+
+        public void Close()
+        {
+            displayed = false;
+            StateHasChanged();
+        }
+    }
+}

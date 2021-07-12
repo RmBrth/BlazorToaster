33cf220ea8a882f5833a9f077bc34909f1c717cf
RmBrth <85992948+RmBrth@users.noreply.github.com>
2021-07-12 12:15:10 +0200

    Add toast container position + migrate style tag properties into css

--- Toaster/Shared/ToastContainer.razor.cs
+++ Toaster/Shared/ToastContainer.razor.cs
@@ -10,11 +10,36 @@ namespace Toaster.Shared
     {
         [Inject] ToastService ToastService { get; set; }
 
+        [Parameter] public ToastContainerPosition Position { get; set; }
+
         private readonly List<Toast> toasts = new();
+        private string position;
 
         protected override void OnInitialized()
         {
             ToastService.OnShow += ShowToast;
+
+            switch (Position)
+            {
+                case ToastContainerPosition.TopLeft:
+                    position = "custom-toast-container-top custom-toast-container-left";
+                    break;
+                case ToastContainerPosition.TopRight:
+                    position = "custom-toast-container-top custom-toast-container-right";
+                    break;
+                case ToastContainerPosition.BottomLeft:
+                    position = "custom-toast-container-bottom custom-toast-container-left";
+                    break;
+                case ToastContainerPosition.BottomCenter:
+                    position = "custom-toast-container-bottom custom-toast-container-center";
+                    break;
+                case ToastContainerPosition.BottomRight:
+                    position = "custom-toast-container-bottom custom-toast-container-right";
+                    break;
+                default:
+                    position = "custom-toast-container-top custom-toast-container-center";
+                    break;
+            }
         }
 
         public void RemoveToast(Guid Id)
@@ -42,5 +67,15 @@ namespace Toaster.Shared
             toasts.Add(toast);
             StateHasChanged();
         }
+    }
+
+    public enum ToastContainerPosition
+    {
+        TopLeft,
+        TopCenter,
+        TopRight,
+        BottomLeft,
+        BottomCenter,
+        BottomRight
     }
 }
